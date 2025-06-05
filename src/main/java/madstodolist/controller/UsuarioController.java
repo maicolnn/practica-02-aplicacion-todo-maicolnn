c0bb9d1737c3aff92a361d03666706f26c00359f
Maicol Nasimba <maicol.nasimba@epn.edu.ec>
2025-06-05 00:23:15 -0500

    Añadido enlace a la descripción y ruta /registrados/{id}

--- src/main/java/madstodolist/controller/UsuarioController.java
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +21,15 @@ public class UsuarioController {
         List<UsuarioData> usuarios = usuarioService.findAll();
         model.addAttribute("usuarios", usuarios);
         return "listaUsuarios";
+    }
+
+    @GetMapping("/registrados/{id}")
+    public String descripcionUsuario(@PathVariable Long id, Model model) {
+        UsuarioData usuario = usuarioService.findById(id);
+        if (usuario == null) {
+            return "redirect:/registrados";
+        }
+        model.addAttribute("usuario", usuario);
+        return "descripcionUsuario";
     }
 }
